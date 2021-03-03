fc84e279c89a268473c87b6f9e15012ae8bf0ec5
andy <andy870622@gmail.com>
2021-03-03 15:14:00 +0800

    Initial Commit

---
+++ lanternGame/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  lanternGame
+//
+//  Created by User15 on 2021/3/3.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ lanternGame/lanternGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  lanternGameApp.swift
+//  lanternGame
+//
+//  Created by User15 on 2021/3/3.
+//
+
+import SwiftUI
+
+@main
+struct lanternGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
